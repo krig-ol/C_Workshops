d1b9bfc53c017478865688f27e8c55fc6d49820b
krig <krig@mail.mail>
2022-08-09 15:44:52 +0300

    first commit

---
+++ homeworks/homework001/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.WriteLine("Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine()); // Вводим с клавиатуры число А
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine()); // Вводим с клавиатуры число В
+
+Console.WriteLine("A = " + a + " " + "B = " + b + " -> " + "max = " + Math.Max(a, b) + " min = " + Math.Min(a, b)); // Выводим в консоль MAX и MIN
+
+Console.WriteLine("Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел");
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.Write("Введите число A1: ");
+int a1 = Convert.ToInt32(Console.ReadLine()); // Вводим с клавиатуры число A1
+Console.Write("Введите число B1: ");
+int b1 = Convert.ToInt32(Console.ReadLine()); // Вводим с клавиатуры число B1
+Console.Write("Введите число C1: ");
+int c1 = Convert.ToInt32(Console.ReadLine()); // Вводим с клавиатуры число C1
+
+Console.WriteLine("A1 = " + a1 + " " + "B1 = " + b1 + " " + "C1 = " + c1 + " -> " + "max = " + Math.Max(a1, Math.Max(b1, c1))); // Выводим в консоль MAX
+
+Console.WriteLine("Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).");
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine()); // Вводим с клавиатуры число
+if (num1 % 2 == 0)
+{
+    Console.WriteLine(num1 + " -> " + "да, число чётное");
+}
+else
+{
+    Console.WriteLine(num1 + " -> " + "нет, число нечётное");
+}
+
+Console.WriteLine("Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine()); // Вводим с клавиатуры число
+int i = 1;
+while (true)
+{
+	if (i % 2 == 0)
+	{
+		Console.Write(i + " ");
+	}
+	if (i >= num2)
+	{
+		break;
+	}
+	i++;
+}
---
+++ workshop001/Program.cs
@@ -0,0 +1,43 @@
+﻿// Console.ReadLine() - команда ввода с клавиатуры
+// Convert.ToInt32 - преобразование строки в число
+// Console.WriteLine() - команда вывода в терминал
+// Типы данных: int(цельночисленный), float(число с плавающей запятой - реализует числа одинарной точности),
+// string(строка), bool(true/false), double(число с плавающей запятой - реализует числа двойной точности);
+
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(number1 + number2);
+
+// Программа "Угадай число"
+
+int secretNumber = new Random().Next(1, 11);
+int attempt = 3;
+bool flagWin = false;
+
+while(attempt > 0)
+{
+    Console.Write("Введите число от одного (1) до десяти (10): ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    attempt -= 1; // attempt--; attempt = attempt - 1
+    if (userNumber > secretNumber)
+    {
+        Console.WriteLine("Секретное число меньше");
+        Console.WriteLine("Попыток осталось " + attempt);
+    }
+    else if (userNumber < secretNumber)
+    {
+        Console.WriteLine("Секретное число больше");
+        Console.WriteLine("Попыток осталось " + attempt);
+    }
+    else
+    {
+        Console.WriteLine("Вы угадали секретное число");
+        flagWin = true;
+        break;
+    }
+}
+if (!flagWin) // !flagWin = flagWin == false
+{
+    Console.WriteLine("Вы проиграли. Секретное число было: " + secretNumber);
+}
