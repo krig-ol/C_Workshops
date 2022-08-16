1bb74602cbaefb3dcc4b044b957f74b7ce2584ee
krig <krig@mail.mail>
2022-08-16 15:37:32 +0300

    add homework003

--- workshop002/Program.cs
+++ workshop002/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Создаем консольную текстовую РПГ
+class Program //Класс Program указывает на принодлежность к файлу Program.cs
+{
+    static void WriteSelect(string[] args) // Функция обработки приведенных значений
+    {
+        for (int i = 0; i < args.Length - 1; i++)
+        {
+            Console.Write(i + 1 + " - " + args[i] + ", ");
+        }
+        Console.WriteLine(args.Length + " - " + args[args.Length - 1] + ".");
+    }
+    static void SelectRace() // Выбор расы
+    {
+        Console.WriteLine("В игре есть персонажи разных рас:");
+        string[] namesRace = { "Люди", "Эльфы", "Орки", "Гоблины", "Гномы" };
+        WriteSelect(namesRace);
+        Console.Write("Выберите вашу расу: ");
+        int numberRace = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ваша раса: " + namesRace[numberRace - 1]);
+    }
+    static void SelectSex() // Выбор пола
+    {
+        Console.WriteLine("В игре есть персонажи двух полов:");
+        string[] selectSex = { "Мужской", "Женский" };
+        WriteSelect(selectSex);
+        Console.Write("Укажите ваш пол: ");
+        int numberSex = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ваш пол: " + selectSex[numberSex - 1]);
+    }
+    static void SelectClass() // Выбор класса
+    {
+        Console.WriteLine("В игре есть несолько классов персонажей:");
+        string[] selectClass = { "Воин", "Маг", "Бард", "Взломщик", "Паладин", "Друид", "Защитник", "Лучник" };
+        WriteSelect(selectClass);
+        Console.Write("Укажите класс вашего персонажа: ");
+        int numberClass = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ваш класс: " + selectClass[numberClass - 1]);
+    }
+     static void Main(string[] args)
+    {
+        SelectRace();
+        SelectSex();
+        SelectClass();
+    }
+}
---
+++ workshop003/Program.cs
@@ -0,0 +1,42 @@
+﻿// && - команда И
+// || - команда ИЛИ
+// bool flag = a > b; // если a > b, то флаг = true, иначе false
+// int input = a > b ? 1 : 2; // переменная input принмает значения: "1" - если "a > b"; и "2" - если "a <= b"; 
+// "?" = "if", замена "{}" - то, что мы будем получать; 
+// ":" = "else";
+
+internal class Program
+{
+    // static int DO(int index)
+    // {
+    //     return index *= index;
+    // }
+    private static void Main(string[] args)
+    {
+        //     int a = 10;
+        //     int b = 15;
+        //     int c = 20;
+        //     int x = 0;
+        //     bool flag = a > b;
+        //     int index = a > b ? DO(a) : DO(b);
+        //     Console.WriteLine(index);
+        //     if (a > b && a > c || a == x)
+        //     {
+
+        //     }
+        while (true) // применяем Math Класс https://docs.microsoft.com/ru-ru/dotnet/api/system.math?view=netcore-1.1
+        {
+            Console.Clear(); // Очищаем консоль
+            Console.WriteLine("Введите любое целое число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Abs(number) + " - Выводим модуль числа " + number + "\n"); // Модуль числа
+            Console.WriteLine(Math.Sqrt(number) + " - Выводим корень квадратный числа " + number + "\n"); // Корень квадратный числа
+            Console.WriteLine(Math.Round(Math.Sqrt(number), 1) + " - Округляем корень квадратный числа " + number + "\n"); // Округление квадратного корня числа, "1" - до одного символа после запятой
+            Console.WriteLine("Введите число, обозначающее степень, в которую мы будем возводить число " + number);
+            double pow = double.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Pow(number, pow) + " - Возводим в степень " + pow + " число " + number + "\n"); // Возведение в степень числа, "pow" - в какую
+            Console.WriteLine("Продолжаем?\nНажми 'y' если ДА или 'n' если НЕТ");
+            if (Console.ReadLine() == "n") break;
+        }
+    }
+}
