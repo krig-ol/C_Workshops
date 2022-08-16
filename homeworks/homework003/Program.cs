f1343acfe90725d8683d08c2c82b82f2b46e4f75
krig <krig@mail.mail>
2022-08-16 15:46:43 +0300

    mod homework003

--- homeworks/homework003/Program.cs
+++ homeworks/homework003/Program.cs
@@ -1,68 +1,68 @@
 ﻿Console.Clear(); // Очищаем консоль
-// Console.WriteLine("Урок 3. Массивы и функции в программировании (Домашнее задание)\n");
+Console.WriteLine("Урок 3. Массивы и функции в программировании (Домашнее задание)\n");
 
-// Console.WriteLine("Задача 19\nНапишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.\n");
+Console.WriteLine("Задача 19\nНапишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.\n");
 
-// // 14212 -> нет
-// // 23432 -> да
-// // 12821 -> да
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
 
-// Console.Write("Введите число: ");
-// M:
-// string? number = Console.ReadLine();
-// Console.WriteLine();
+Console.Write("Введите число: ");
+M:
+string? number = Console.ReadLine();
+Console.WriteLine();
 
-// void CheckingNumber(string number)
-// {
-//     if (number[0] == number[4] || number[1] == number[3])
-//     {
-//         Console.WriteLine($"Ваше число: {number} - палиндром.\n");
-//     }
-//     else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.\n");
-// }
+void CheckingNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Ваше число: {number} - палиндром.\n");
+    }
+    else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.\n");
+}
 
-// if (number!.Length == 5)
-//     {
-//         CheckingNumber(number);
-//     }
-// while (number!.Length < 5 || number!.Length > 5)
-//     {
-//         Console.Write($"Введите пятизначное число: ");
-//         goto M;
-//     }
+if (number!.Length == 5)
+    {
+        CheckingNumber(number);
+    }
+while (number!.Length < 5 || number!.Length > 5)
+    {
+        Console.Write($"Введите пятизначное число: ");
+        goto M;
+    }
 
 
 
-// Console.WriteLine("Задача 21:\nНапишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.\n");
+Console.WriteLine("Задача 21:\nНапишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.\n");
 
-// // A (3,6,8); B (2,1,-7), -> 15.84
-// // A (7,-5, 0); B (1,-1,9) -> 11.53
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// int x1 = Coordinate("X", "A");
-// int y1 = Coordinate("Y", "A");
-// int z1 = Coordinate("Z", "A");
-// int x2 = Coordinate("X", "B");
-// int y2 = Coordinate("Y", "B");
-// int z2 = Coordinate("Z", "B");
+int x1 = Coordinate("X", "A");
+int y1 = Coordinate("Y", "A");
+int z1 = Coordinate("Z", "A");
+int x2 = Coordinate("X", "B");
+int y2 = Coordinate("Y", "B");
+int z2 = Coordinate("Z", "B");
 
-// int Coordinate(string coorName, string pointName)
-// {
-//     Console.Write($"Введите координату {coorName} точки {pointName}: ");
-//     return Convert.ToInt32(Console.ReadLine());
-// }
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-// double Decision(double x1, double x2,
-//                 double y1, double y2,
-//                 double z1, double z2)
-// {
-//     return Math.Sqrt(Math.Pow((x2 - x1), 2) +
-//                      Math.Pow((y2 - y1), 2) +
-//                      Math.Pow((z2 - z1), 2));
-// }
+double Decision(double x1, double x2,
+                double y1, double y2,
+                double z1, double z2)
+{
+    return Math.Sqrt(Math.Pow((x2 - x1), 2) +
+                     Math.Pow((y2 - y1), 2) +
+                     Math.Pow((z2 - z1), 2));
+}
 
-// double segmentLength = Math.Round(Decision(x1, x2, y1, y2, z1, z2), 2);
+double segmentLength = Math.Round(Decision(x1, x2, y1, y2, z1, z2), 2);
 
-// Console.WriteLine($"\nДлина отрезка = {segmentLength}\n");
+Console.WriteLine($"\nДлина отрезка = {segmentLength}\n");
 
 
 
@@ -72,15 +72,15 @@ Console.WriteLine("Задача 23:\nНапишите программу, кот
 // 5 -> 1, 8, 27, 64, 125
 
 Console.Write("\nВведите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int numb = Convert.ToInt32(Console.ReadLine());
 
-void Cube(int[] number)
+void Cube(int[] numb)
 {
     int cube = 1;
-    int length = number.Length;
+    int length = numb.Length;
     while (cube < length)
     {
-        number[cube] = Convert.ToInt32(Math.Pow(cube, 3));
+        numb[cube] = Convert.ToInt32(Math.Pow(cube, 3));
         cube++;
     }
 }
@@ -96,6 +96,6 @@ void PrintArray(int[] mass)
     }
 }
 
-int[] array = new int[number + 1];
+int[] array = new int[numb + 1];
 Cube(array);
 PrintArray(array);
