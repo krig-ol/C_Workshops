9531ed18fba23315f21220e1f813878691918988
krig <krig@mail.mail>
2022-08-25 16:49:15 +0300

    add workshop006 + homework006

---
+++ homeworks/homework006/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.Clear();
+
+Console.WriteLine("Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.\n");
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите число, задающее количество чисел, вводимых с клавиатуры: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+int[] arrayM = new int[numberM];
+
+void InputNumbers(int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"Введите {i + 1}е число: ");
+        arrayM[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int GreaterThanZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+
+InputNumbers(numberM);
+
+Console.WriteLine($"Чисел больше нуля было введено: {GreaterThanZero(arrayM)} \n");
+
+
+Console.WriteLine("Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,"
++ " заданных уравнениями: \ny = k1 * x + b1, \ny = k2 * x + b2; \nзначения b1, k1, b2 и k2 задаются пользователем.\n");
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine($"\nДве прямые параллельны.\n");
+}
+else
+{
+    double x = Math.Round(((-b2 + b1) / (-k1 + k2)), 1);
+    Console.WriteLine($"x = (-b2 + b1) / (-k1 + k2) = {x}");
+    double y = Math.Round((k2 * x + b2), 1);
+    Console.WriteLine($"y = k2 * x + b2 = {y}");
+
+    Console.WriteLine($"\nДве прямые пересекаются в точке с координатами (X: {x}; Y: {y})\n");
+}
---
+++ workshop006/Program.cs
@@ -0,0 +1,85 @@
+﻿// Урок 6. Двумерные массивы и рекурсия
+
+Console.Clear();
+Console.WriteLine("Контрольная работа");
+
+Console.WriteLine("Задача 35:\nЗадайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].\n");
+
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] array = new int[123];
+Random random = new Random();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 300);
+    if (array[i] > 10 && array[i] < 100)
+        count++;
+}
+static void BubbleSort(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        for (int j = 0; j < Array.Length - 1; j++)
+        {
+            if (Array[j] > Array[j + 1])
+            {
+                int temp = Array[j];
+                Array[j] = Array[j + 1];
+                Array[j + 1] = temp;
+            }
+        }
+    }
+}
+BubbleSort(array);
+Console.WriteLine("В массиве из 123 случайных чисел:\n[{0}]\n" + $"количество элементов массива, значения которых лежат в отрезке от 10 до 99 равно '{count}'\n", string.Join(", ", array));
+
+
+Console.WriteLine("Задача 37:\nНайдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.\n");
+
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+Console.Write("Введите чётное число, задающее количество элементов массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] NewArray(int number, int min, int max)
+{
+    int[] array = new int[number];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+
+    return array;
+}
+string PrintArray(int[] array)
+{
+    string result = string.Empty;
+    result += string.Join(", ", array);
+    return result;
+}
+int[] mass = NewArray(number, 1, 10);
+
+string Multiplicator(int[] array)
+{
+    string result = string.Empty;
+    int multiplication = 0;
+    int j = array.Length - 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length / 2)
+        {
+            multiplication = array[i] * array[j];
+            result += $"{array[i]} x {array[j]} = {multiplication}; ";
+            j--;
+        }
+    }
+    return result;
+}
+Console.WriteLine("В одномерном массиве:\n[" + PrintArray(mass) + $"]\nпроизведением пар чисел будет\n{Multiplicator(mass)}\n");
